30aa24eab5e7a66ba45d355b50f32320c57346a4
TheusBr05 <mat.587263@gmail.com>
2025-05-19 17:21:46 -0300

    Refatora modelo e rotas de usuário: renomeia arquivo de rotas, ajusta prefixo da URL e melhora a definição da tabela no modelo

--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -1,9 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from . import db
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
