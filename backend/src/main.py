4d107992461ae4c332c676c2828486c83afc92ef
TheusBr05 <mat.587263@gmail.com>
2025-05-19 17:22:19 -0300

    Adiciona suporte a rotas de usuário e configura CORS para rotas específicas

--- backend/src/main.py
+++ backend/src/main.py
@@ -5,23 +5,28 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 from flask import Flask, send_from_directory, request # Added request
 from flask_cors import CORS # Added CORS import
+from flask_migrate import Migrate
 from src.models import db
 from src.routes.task_routes import task_bp
 from dotenv import load_dotenv
+from src.routes.user_routes import user_bp  # Added user routes
 
 load_dotenv()
 
 app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), 'static'))
-CORS(app) # Enabled CORS for all routes
+CORS(app, resources={r"/api/*": {"origins": "*"}}) # Enabled CORS for all routes
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 app.register_blueprint(task_bp, url_prefix='/api/tasks')
+app.register_blueprint(user_bp, url_prefix='/api/users')  # Added user routes
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 with app.app_context():
     db.create_all()
+
+migrate = Migrate(app, db)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
